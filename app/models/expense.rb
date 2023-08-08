8585f9774592cba6d5954b7915f22dd1d46d83a5
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-08 18:28:53 +0300

    Add validation to category and expense models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :category_expenses, dependent: :destroy
   has_many :categories, through: :category_expenses
+
+  validates :name, :amount, presence: true
+  validates :amount, numericality: true, comparison: { greater_than_or_equal_to: 0 }
 end
