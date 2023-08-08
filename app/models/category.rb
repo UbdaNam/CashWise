1b8b79803aeccf619735b06b9adf12874e6aba2d
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-08 18:07:09 +0300

    Add category model

---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
+  has_many :categories, foreign_key: :author_id, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
