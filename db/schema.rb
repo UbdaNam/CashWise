62ae990af53c05aa118087af0fd25ce50dcd0910
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-08 18:22:18 +0300

    Add join table for category and expense

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_150334) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_151503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_150334) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "category_expenses", force: :cascade do |t|
+    t.bigint "expense_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_expenses_on_category_id"
+    t.index ["expense_id"], name: "index_category_expenses_on_expense_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -53,5 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_150334) do
   end
 
   add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "category_expenses", "categories"
+  add_foreign_key "category_expenses", "expenses"
   add_foreign_key "expenses", "users", column: "author_id"
 end
