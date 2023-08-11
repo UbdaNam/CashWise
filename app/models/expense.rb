d0958094c9c85e583dead2c20a78df94221df3ef
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-11 13:59:08 +0300

    Add template UI for category and transactions

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :category_expenses, dependent: :destroy
-  has_many :categories, through: :category_expenses
+  has_many :categories, through: :category_expenses, dependent: :destroy
 
   validates :name, :amount, presence: true
   validates :amount, numericality: true, comparison: { greater_than_or_equal_to: 0 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "welcome#index"
+
+  resources :categories, only: [:index, :show, :new, :create]
+  resources :expenses, only: [:new, :create]
 end
