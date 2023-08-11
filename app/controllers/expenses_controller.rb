79a51b42d257a9599c9b0367eec1e22b35c56389
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-11 22:06:07 +0300

    Add dynamic pages

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,23 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @expense = Expense.new
+    @categories = current_user.categories
   end
 
   def create
+    @expense = Expense.new(expense_params)
+    @expense.author = current_user
+    p :params
+    if @expense.save
+      redirect_to @expense.categories.first, notice: "Transaction created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount, category_ids:[])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ApplicationRecord
   has_many :expenses, through: :category_expenses, dependent: :destroy
 
   validates :name, :icon, presence: true
+
+  def ordered_by_recent
+    expenses.order(created_at: :desc)
+  end
 end
